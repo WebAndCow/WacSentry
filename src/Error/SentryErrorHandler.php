38739149c54a8091947d4b38ac2e4aa88920d600
PierreWAC <p.barbier@webandcow.com>
2020-07-07 12:11:50 +0200

    -- add production_only for no middleware ErrorHandler

--- src/Error/SentryErrorHandler.php
+++ src/Error/SentryErrorHandler.php
@@ -3,11 +3,15 @@ namespace WacSentry\Error;
 use Exception;
 
 use Cake\Error\ErrorHandler as ErrorHandler;
+use Cake\Core\Configure;
 
 class SentryErrorHandler extends ErrorHandler {
     public function handleException(Exception $exception)
     {
-        \Sentry\captureException($exception);
+        // Si production_only == false ou qu'on n'est pas en debug
+        if (Configure::read('Sentry.production_only') === false || !Configure::read('debug')) {
+            \Sentry\captureException($exception);
+        }
 
         parent::handleException($exception);
     }
